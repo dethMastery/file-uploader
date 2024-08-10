d1d0f71e76b5630d3f51b0259219bf7ba78dc4ab
Suphakit P <me@suphakit.net>
2024-08-11 00:28:03 +0700

    feat<backend>: upload api based

--- src/pages/upload/index.js
+++ src/pages/upload/index.js
@@ -1,11 +1,40 @@
-async function UploadPage(req, res) {
-  console.log(req.file)
+const fs = require('fs')
 
-  if (req.file == undefined) {
-    res.send('blank')
+async function statusFunction(file, path) {
+  let message
+
+  if (file != undefined) {
+    console.log(file)
+    console.log(`${path}/${file.path}`)
+
+    fs.renameSync(
+      `${path}/${file.path}`,
+      `${path}/uploads/${file.originalname}`
+    )
+
+    message = {
+      status: 1,
+      message: 'upload complete',
+      payload: {
+        url: `${process.env.CDN_URI}${file.originalname}`,
+      },
+    }
   } else {
-    res.send('a')
+    message = {
+      status: 0,
+      message: 'upload incomplete',
+    }
   }
+
+  return message
+}
+
+async function UploadPage(req, res, path) {
+  const rtnPayload = {
+    status: await statusFunction(req.file, path),
+  }
+
+  res.send(rtnPayload)
 }
 
 module.exports = UploadPage
